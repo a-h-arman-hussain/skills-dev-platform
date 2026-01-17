9fd721043e2f627f7d19403630bcd7d4c74d88c6
a-h-arman-hussain <armanhd16@gmail.com>
2026-01-17 15:27:50 +0600

    deploy issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next"; // আপনি এটি ইম্পোর্ট করেছেন
+import nextCoreWebVitals from "eslint-config-next";
 
 const eslintConfig = defineConfig([
-  ...nextCoreWebVitals, // এখানে 'nextVitals' এর বদলে 'nextCoreWebVitals' ব্যবহার করুন
+  ...nextCoreWebVitals,
 
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
